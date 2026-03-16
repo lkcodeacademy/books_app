df44f001888c67247e507e210933cee11de93bb5
Povilas <povilas110@gmail.com>
2026-03-16 21:41:44 +0200

    fix(terraform): convert github_actions role to data source and remove policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,46 +101,8 @@ data "aws_iam_openid_connect_provider" "github" {
 }
 
 # Role assumed by GitHub Actions when running terraform.
-resource "aws_iam_role" "github_actions" {
+data "aws_iam_role" "github_actions" {
   name = "github-actions-terraform"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = data.aws_iam_openid_connect_provider.github.arn
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:lkcodeacademy/books_app:ref:refs/heads/*"
-          }
-        }
-      }
-    ]
-  })
-}
-
-# Attach a policy with permissions needed by Terraform
-resource "aws_iam_role_policy" "github_actions_policy" {
-  name = "github-actions-terraform-policy"
-  role = aws_iam_role.github_actions.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "ec2:*",
-          "rds:*",
-          "ecr:*",
-          "iam:PassRole"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
 }
 
 
