9e6e4863063595614f329ba7634385cb6d732331
Povilas <povilas110@gmail.com>
2026-03-17 19:10:56 +0200

    fix(terraform): open ports 8080 and 5001 in security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,6 +104,20 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 5001
+    to_port = 5001
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
